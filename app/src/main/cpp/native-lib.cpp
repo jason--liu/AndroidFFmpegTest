ea765dca0a55f02a2264be9e96402b90753de46d
liuqianyi <liuqianyi1125@gmail.com>
2019-10-09 23:15:18 +0800

    添加片元着色器glsl

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -253,7 +253,7 @@ Java_com_example_androidffmpegtest_MainActivity_testopenssl(JNIEnv *env, jobject
     //env->ReleaseStringUTFChars(path_, path);
 }
 
-//定点着色器glsl
+//顶点着色器glsl
 #define GET_STR(x) #x //将传入的x直接转换为字符串且加了引号，比较清晰
 static const char *vertexShader = GET_STR(
         attribute vec4 aPosition;//顶点坐标？
@@ -265,6 +265,28 @@ static const char *vertexShader = GET_STR(
         }
 );//参考ijkplay
 
+// 片元着色器
+// p表示平面存储，即Y存完了再存U,V ffmpeg软解码和部分x86硬解码出来的格式
+static const char *fragYUV420p = GET_STR(
+    precision mediump float;//精度
+    varying vec2 vTexCoord;//顶点着色器传递的坐标
+    // 三个输入参数，输入材质（灰度材质，单像素）
+    uniform sampler2D yTexture;
+    uniform sampler2D uTexture;
+    uniform sampler2D vTexture;
+    void main(){
+        vec3 yuv;
+        vec3 rgb;
+        yuv.r = texture2D(yTexture,vTexCoord).r;
+        yuv.g = texture2D(uTexture,vTexCoord).r-0.5;
+        yuv.b = texture2D(vTexture,vTexCoord).r-0.5;
+        rgb  = mat3(1.0,  1.0,   1.0
+                    0.0, -0.39465,2.03221
+                    1.13983, -0.5806, 0.0)*yuv;
+        //输出像素颜色
+        gl_FragColor = vec4(rgb,1.0);
+    }
+);
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_example_androidffmpegtest_XPlay_Open(JNIEnv *env, jobject instance, jstring url_,
